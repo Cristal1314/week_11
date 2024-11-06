603d3430e3408feba6100bb532190148a981eabc
Jimenez <CFJimenez@cps.k12.il.us>
2024-11-06 09:32:36 -0600

    first commit

---
+++ challnge.py
@@ -0,0 +1,92 @@
+# #Student Grade Categorization
+
+# score1 = int(input("Enter your score: ")) # Asked the user to input the first score that they got
+
+# score2 = int(input("Another score?: ")) # To input another score
+
+# score3 = int(input("Another score?: "))# To input another score
+
+
+
+# # for first score
+# while score2 <= 90: #if score is above or equal to 90,
+#     print("Excellent") #Excellent will be printed
+#     if score2 >= 70 <= 89: #if score is above or equal 70 and less tha
+#         print("Good")
+#     elif score2 >= 50 <= 69:
+#         print("Pass")
+#     elif score2 <= 50:
+#         print("Fail")
+#     else:
+#         print()
+
+# # for second score
+# while score2 <= 90: #if score is above or equal to 90,
+#     print("Excellent") #Excellent will be printed
+#     if score2 >= 70 <= 89: #if score is above or equal 70 and less tha
+#         print("Good")
+#     elif score2 >= 50 <= 69:
+#         print("Pass")
+#     elif score2 <= 50:
+#         print("Fail")
+#     else:
+#         print()
+
+# # for third score
+# while score3 <= 90: #if score is above or equal to 90,
+#     print("Excellent") #Excellent will be printed
+#     if score3 >= 70 <= 89: #if score is above or equal 70 and less tha
+#         print("Good")
+#     elif score3 >= 50 <= 69:
+#         print("Pass")
+#     elif score3 <= 50:
+#         print("Fail")
+#     else:
+#         print()
+
+
+#-----------------------------------------------------------------------------------------------
+#Even and Odd Counter with Canditions
+
+ 
+#Ask the user for a Starting and ending number
+starting_num= int(input("Enter a starting number 1 :"))
+ending_num=int(input("Enter a ending number from :"))
+e
+#check if the number is both even and greater then 10
+#check if tge number is odd and less then 20
+print (starting_num and ending_num)
+for x in range(starting_num,ending_num):
+    if starting_num % 2 == 0:
+        print("Your number is even.")
+        if starting_num > 10:
+            print("Your number is greater than 10.")
+    elif starting_num % 2 == 1:
+        print("Your number is odd.")
+        if starting_num > 10:
+            print("Your number is greater than 10.")
+    elif ending_num % 2 == 0:
+        print("Your number is even.")
+    if ending_num > 10:
+            print("Your number is greater than 10.")
+    elif ending_num % 2 == 1:
+        print("Your number is odd.")
+    if ending_num <20:
+            print("Your number is less then 20 .")
+
+
+#print the counts for both "special odd "   and "special even" numbers
+            
+                 
+If ending_num is ending_num %2==0 and ending_num >10 : 
+print ("It is a special even number:" + str(ending_num))
+If starting_num is starting_num%2==0 and starting_num>10:
+print("It is a special even number :" +str(starting_num))
+
+If ending_num is ending_num<20 and ending_num %2==1 :
+print ("It is a special odd number:"= str(ending_num))
+If starting_num is starting_num<20 and starting_num %2==1 :
+    #print ("It is a special odd number:"= str(starting_num))
+    
+ 
+ 
